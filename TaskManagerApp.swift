01cf297d7fc32d1481a28ad814149ab9cefa3ffa
TREMPLIX <64973286+TREMPLIX@users.noreply.github.com>
2025-05-16 16:58:56 +0300

    Add files via upload

---
+++ TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by SERGEY TREMPLIX on 23.04.2025.
+//
+
+import SwiftUI
+
+//@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
